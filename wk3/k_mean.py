bce57023b295660aa51f3a3efdaa2a8ca1e80af3
Yuma nacamurura <yuma.nacamura@gmail.com>
2019-01-11 16:52:18 +0900

    fix to move

--- wk3/k_mean.py
+++ wk3/k_mean.py
@@ -1,34 +1,40 @@
-import collections
+import sys
 
-k = 5
+arg = sys.argv
+if '-k' in arg:
+  k = int(arg[arg.index('-k')+1])
+else:
+  k = 5
 
 def get_vec(filename):
     with open(filename,'r') as f:
-        sens = f.read().split('\n')
+        sens = f.read()[:-1].split('\n')
     return [{word:float(tfidf) for word,tfidf in map(lambda x:x.split(':'),sen.split(' '))} for sen in sens]
 
 def cos_sim(v1,v2):
     key = set(v1.keys()) | set(v2.keys())
     vec = [[v[k] if k in v else 0 for k in key] for v in [v1,v2]]
-    norm = (lambda x:(x[0]*x[1])([sum(map(lambda x:x**2,v)) for v in vec])**0.5
-    return sum(map(lambda x:x[0]*x[1],zip(vec*)))/norm
+    norm = (lambda x:(x[0]*x[1]))([sum(map(lambda x:x**2,v)) for v in vec])**0.5
+    return sum(map((lambda x:x[0]*x[1]),zip(*vec)))/norm
 
 def annotate(data,label):
     return list(map(lambda x:(x,label),data))
 
 suf = ['cleaner','mp3player','test']
-clnr_vec,mp3_vec,test_vec = [get_vec('../vec/{0}.vec=tfidf.txt'.format(name)) for name in suf]
+clnr_vec,mp3_vec,test_vec = [get_vec('../k_mean/{0}.vec=tfidf.txt'.format(name)) for name in suf]
 
 teacher_vec = annotate(clnr_vec,'cleaner') + annotate(mp3_vec,'mp3')
 
 ans = []
 
 for vec in test_vec:
-    teacher_vec.sort(lambda x:cos_sim(x[0],vec))
-    if len(filter(lambda x:x[1]=='cleaner',teacher_vec[:k])) > k/2:
+    teacher_vec.sort(key = (lambda x:-cos_sim(x[0],vec)))
+    #print(cos_sim(teacher_vec[0][0],vec))
+    if len(list(filter(lambda x:x[1]=='cleaner',teacher_vec[:k]))) > k/2:
         ans.append('cleaner')
     else:
-        ans.append('mp3')
+        ans.append('mp3player')
 
 with open('../k_mean/output.txt','w') as f:
     f.write('\n'.join(ans))
+
